96fad6c25ce306357cc65561479347465ac75667
sneha-n-333 <sneha333n@gmail.com>
2024-06-15 11:55:14 +0530

    added

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from django.urls import path
 from django_distill import distill_path
 from . import views
@@ -21,22 +21,3 @@ urlpatterns = [
     path('remove_cart/<str:cid>',views.remove_cart,name="remove_cart"),
     path('addtocart',views.add_to_cart,name="addtocart"),
 ]
-=======
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.home,name="home"),
-    path('menu',views.menu,name="menu"),
-    path('register',views.register,name="register"),
-    path('login',views.login_page,name="login"),
-    path('logout',views.logout_page,name="logout"),
-    path('menu/<str:name>',views.menuview,name="menu"),
-    path('menu/<str:cname>/<str:pname>',views.product_details,name="product_details"),
-    path('about',views.about,name="about"),
-    path('order',views.order,name="order"),
-    path('cart',views.cart_page,name="cart"),
-    path('remove_cart/<str:cid>',views.remove_cart,name="remove_cart"),
-    path('addtocart',views.add_to_cart,name="addtocart"),
-]
->>>>>>> 7c21ed5aeb93418d1ab75479e42e840677e8decc
